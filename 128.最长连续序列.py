397b47ffa6e0a6e25d1bf3a57795016062e0e1db
lilgrey23 <7339631@qq.com>
2026-02-23 13:18:59 +0800

    Add files via upload

---
+++ 128.最长连续序列.py
@@ -0,0 +1,26 @@
+#128.最长连续序列
+#给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+#请你设计并实现时间复杂度为 O(n) 的算法解决此问题
+#示例 1：
+#输入：
+#nums = [100,4,200,1,3,2]
+#输出：
+#4
+#解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums)#去重
+
+        for num in num_set:
+            if num - 1 not in num_set:#判断跳过不是序列起点的数
+                current_num =num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
